672b8be4a7a259846c7de854801fc88e52d81144
DanGiaime <dsg6874@rit.edu>
2020-12-10 11:17:12 -0600

    Reject locations that don't have yelp. Fix description gathering + async issues. Filter for only locations with yelp image_url

--- apiMethods.js
+++ apiMethods.js
@@ -1,42 +1,50 @@
-const fetch = require('node-fetch');
-const dotenv = require('dotenv');
-const {project} = require('./helpers');
+const fetch = require("node-fetch");
+const dotenv = require("dotenv");
+const { project } = require("./helpers");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 dotenv.config();
 
 // Replace these with your actual API codes
 // I used dotenv to help, so they're set to my .env file
 const YELP_API = process.env.YELP_API;
 const GOOGLE_API = process.env.GOOGLE_API;
+const GOOGLE_SEARCH_API = process.env.GOOGLE_SEARCH_API;
+const GOOGLE_SEARCH_CX = process.env.GOOGLE_SEARCH_CX;
 
 const CHICAGO_BIZ_ENDPOINT =
-  'https://data.cityofchicago.org/resource/r5kz-chrr.json';
+  "https://data.cityofchicago.org/resource/r5kz-chrr.json";
 
 const YELP_BUSINESS_ENDPOINT =
-  'https://api.yelp.com/v3/businesses/matches?city=chicago&state=il&country=US';
-const YELP_ID_ENDPOINT = 'https://api.yelp.com/v3/businesses/';
+  "https://api.yelp.com/v3/businesses/matches?city=chicago&state=il&country=US";
+const YELP_ID_ENDPOINT = "https://api.yelp.com/v3/businesses/";
 
 const GOOGLE_PLACES_SEARCH_ENDPOINT =
-  'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&inputtype=textquery';
+  "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&inputtype=textquery";
 const GOOGLE_PLACES_DETAILS_ENDOINT =
-  'https://maps.googleapis.com/maps/api/place/details/json?&';
+  "https://maps.googleapis.com/maps/api/place/details/json?&";
 
 // ALL THE BELOW METHODS RETURN THE JSON DATA RECEIVED FROM THE APIs
 
 // Need to add search value and search parameter, as function parameters
 // Then write a filter statement based off it
-const fetchFromChicagoBiz = async (searchField, searchValue, desiredFields = undefined) => {
-  console.log('Searching Chicago Business for data....');
+const fetchFromChicagoBiz = async (
+  searchField,
+  searchValue,
+  desiredFields = undefined
+) => {
+  console.log("Searching Chicago Business for data....");
 
   // Right now I'm requiring a searchField and searchValue, but that can change
   if (!searchField || !searchValue) {
-    console.error('Missing searchField and/or searchValue');
+    console.error("Missing searchField and/or searchValue");
     return;
   }
 
   let chicagoDump = await fetch(
     `${CHICAGO_BIZ_ENDPOINT}?${searchField}=${searchValue}`,
     {
-      method: 'get',
+      method: "get",
     }
   )
     .then((res) => res.json())
@@ -46,26 +54,28 @@ const fetchFromChicagoBiz = async (searchField, searchValue, desiredFields = und
     `Found Chicago Business details for ${searchField} : ${searchValue}`
   );
 
-  if(desiredFields) {
-    chicagoDump = chicagoDump.map(singleBiz => project(singleBiz, desiredFields));
+  if (desiredFields) {
+    chicagoDump = chicagoDump.map((singleBiz) =>
+      project(singleBiz, desiredFields)
+    );
   }
   if (chicagoDump) return chicagoDump;
 };
 
 // businessData - Right now the method runs on the assumption that businessData contains a name and an address
 const fetchFromYelp = async (businessData, desiredFields = undefined) => {
-  console.log('Searching Yelp for data ....');
+  console.log("Searching Yelp for data ....");
   if (!businessData) {
-    console.error('Missing searchField and/or searchValue');
+    console.error("Missing searchField and/or searchValue");
     return;
   }
   // Basically just to grab the business id
   let yelpData = await fetch(
     `${YELP_BUSINESS_ENDPOINT}&name=${businessData.name}&address1=${businessData.address}`,
     {
-      method: 'get',
+      method: "get",
       headers: {
-        Authorization: 'Bearer ' + YELP_API,
+        Authorization: "Bearer " + YELP_API,
       },
     }
   )
@@ -74,11 +84,14 @@ const fetchFromYelp = async (businessData, desiredFields = undefined) => {
 
   // Searches by id to get even MORE info
   const businessId = yelpData?.businesses?.[0]?.id;
+  if(yelpData?.businesses?.length == 0) {
+    return businessData;
+  }
   try {
     yelpData = await fetch(`${YELP_ID_ENDPOINT}${businessId}`, {
-      method: 'get',
+      method: "get",
       headers: {
-        Authorization: 'Bearer ' + YELP_API,
+        Authorization: "Bearer " + YELP_API,
       },
     })
       .then((res) => res.json())
@@ -86,16 +99,18 @@ const fetchFromYelp = async (businessData, desiredFields = undefined) => {
 
     if (yelpData && !yelpData.error) {
       console.log(`Found yelp details for ${businessData.name}`);
-      if(desiredFields) {
+      if (desiredFields) {
         yelpData = project(yelpData, desiredFields);
       }
       return {
         yelp: yelpData,
-        ...businessData
+        ...businessData,
       };
     }
 
-    console.log(`Found to find yelp details for ${businessData.name}: ${yelpData?.error?.description} (${yelpData?.error?.code})`);
+    console.log(
+      `Failed to find yelp details for ${businessData.name}: ${yelpData?.error?.description} (${yelpData?.error?.code})`
+    );
     return businessData;
   } catch (err) {
     console.error(
@@ -107,11 +122,11 @@ const fetchFromYelp = async (businessData, desiredFields = undefined) => {
 
 // businessData - Right now the method runs on the assumption that businessData contains a name and an address
 const fetchFromGoogle = async (businessData, desiredFields = undefined) => {
-  console.log('Searching google for place data....');
+  console.log("Searching google for place data....");
   let googleData = await fetch(
     `${GOOGLE_PLACES_SEARCH_ENDPOINT}&key=${GOOGLE_API}&input=${businessData.name} ${businessData.address}`,
     {
-      method: 'get',
+      method: "get",
     }
   )
     .then((res) => res.json())
@@ -122,10 +137,10 @@ const fetchFromGoogle = async (businessData, desiredFields = undefined) => {
     console.log(`Found google place id for ${businessData.name}: ${placeId}`);
 
     // Searches by id to get even MORE info
-    if(placeId) {
+    if (placeId) {
       googleData = await fetch(
         `${GOOGLE_PLACES_DETAILS_ENDOINT}&key=${GOOGLE_API}&place_id=${placeId}`,
-        { method: 'get' }
+        { method: "get" }
       )
         .then((res) => res.json())
         .catch((err) => console.error(err));
@@ -138,17 +153,59 @@ const fetchFromGoogle = async (businessData, desiredFields = undefined) => {
     return businessData;
   }
   console.log(`Found google place details for ${businessData.name}`);
-  if(googleData?.result && desiredFields) {
+  if (googleData?.result && desiredFields) {
     googleData.result = project(googleData.result, desiredFields);
   }
   return {
-    google: (googleData.result),
-    ...businessData
+    google: googleData.result,
+    ...businessData,
   };
+};
+
+// const fetchDescriptionFromGoogleSearch = async (bizData) => {
+
+//   const exampleData = '"606 salon chicago"'
+//   const url = `https://www.googleapis.com/customsearch/v1?key=${encodeURIComponent(GOOGLE_SEARCH_API)}&cx=${encodeURIComponent(GOOGLE_SEARCH_CX)}&q=${encodeURIComponent(exampleData)}`;
+
+//   const searchData = await fetch(
+//     url,
+//     { method: 'get' }
+//   )
+//     .then((res) => res.json())
+//     .catch((err) => console.error(err));
+
+//     const tempProjectedFields = {
+//       "pagemap": ""
+//     };
+
+//   return searchData.items.map(x => x.pagemap.metatags[0]["og:description"]);
+//   return searchData;
+// }
+
+const fetchDescriptionFromYelpPage = async (bizData) => {
+  const url = bizData?.details?.yelp_url;
+  //console.log(url);
+  if (url != undefined) {
+    return fetch(url, { method: "get" })
+      .then((res) => res.text())
+      .then((html) => {
+        const dom = new JSDOM(html);
+        //console.log(dom.window.document.querySelector('meta[property="og:description"]').content); // "Hello world"
+  
+        return dom.window.document.querySelector('meta[property="og:description"]').content;
+      })
+      .catch((err) => console.error(err));
+      
+  }
+  else {
+    console.log("no description found for: " + bizData.name);
+    return "no-description";
+  }
 };
 
 module.exports = {
   fetchFromChicagoBiz,
   fetchFromYelp,
   fetchFromGoogle,
+  fetchDescriptionFromYelpPage,
 };
--- main.js
+++ main.js
@@ -2,31 +2,35 @@ const {
   fetchFromChicagoBiz,
   fetchFromGoogle,
   fetchFromYelp,
+  fetchDescriptionFromYelpPage,
 } = require("./apiMethods");
 
 // Business that i'll test on Chicago Business
 const chicagoSearchField = "zip_code";
 const chicagoSearchValue = "60606";
 const desiredYelpFields = {
-  "categories": "",
-  "is_claimed": "",
-  "cross_streets": "",
-  "coordinates": "",
-  "photos": "",
-  "image_url": "",
-  "hours": "",
+  categories: "",
+  is_claimed: "",
+  location: "",
+  coordinates: "",
+  photos: "",
+  image_url: "",
+  hours: "",
   "display-phone": "",
-  "url": "",
+  url: "",
+  rating: "",
+  "review_count": "",
+  price: "",
 };
 const desiredGoogleFields = {
-  "address_components": "",
-  "business_status": "",
-  "permanently_closed": "",
-  "formatted_phone_number": "",
-  "photos": "",
-  "types": "",
-  "hours": "",
-  "website": "",
+  address_components: "",
+  business_status: "",
+  permanently_closed: "",
+  formatted_phone_number: "",
+  photos: "",
+  types: "",
+  hours: "",
+  website: "",
 };
 
 const desiredChicagoBizFields = {
@@ -43,20 +47,21 @@ const desiredChicagoBizFields = {
   zip_code: "",
   doing_business_as_name: "",
   legal_name: "",
+  address: "",
 };
-
 
 //GOAL
 // Input: zip code
 // Output: as much info as possible for each business
 
-const delay = t => new Promise(resolve => setTimeout(resolve, t));
+const delay = (t) => new Promise((resolve) => setTimeout(resolve, t));
 
 const buildCategories = (allProjectedBusinessData) => {
-  return allProjectedBusinessData.map(currentData => {
+  return allProjectedBusinessData.map((currentData) => {
     return {
-      "location": {
-        cross_streets: currentData?.yelp?.cross_streets,
+      location: {
+        address: currentData?.address,
+        cross_streets: currentData?.yelp?.location?.cross_streets,
         coordinates: currentData?.yelp?.coordinates,
         address_components: currentData?.google?.address_components,
         chicago_license_location: currentData?.chicago?.location,
@@ -67,72 +72,86 @@ const buildCategories = (allProjectedBusinessData) => {
         "ward-precinct": currentData?.chicago?.["ward-precinct"],
         city: currentData?.chicago?.city,
         state: currentData?.chicago?.state,
-        zip_code: currentData?.chicago?.zip_code
+        zip_code: currentData?.chicago?.zip_code,
       },
-      "contact": {
+      contact: {
         "display-phone": currentData?.yelp?.["display-phone"],
         formatted_phone_number: currentData?.google?.formatted_phone_number,
         website: currentData?.google?.website,
-
       },
-      "details": {
+      details: {
+        name: currentData?.name,
         hours: currentData?.yelp?.hours,
         is_claimed_on_yelp: currentData?.yelp?.is_claimed,
         yelp_hours: currentData?.yelp?.hours,
         yelp_url: currentData?.yelp?.url,
         yelp_tags: currentData?.yelp?.categories,
         business_status: currentData?.google?.business_status,
-        permanently_closed: currentData?.google?.permanently_closed,
         google_tags: currentData?.google?.types,
         google_hours: currentData?.google?.hours,
         business_activity: currentData?.chicago?.business_activity,
         license_description: currentData?.chicago?.license_description,
         doing_business_as_name: currentData?.chicago?.doing_business_as_name,
-        legal_name: currentData?.chicago?.legal_name
+        legal_name: currentData?.chicago?.legal_name,
       },
-      "photos": {
+      photos: {
         image_url: currentData?.yelp?.image_url,
         yelp_photos: currentData?.yelp?.photos,
-        google_photos: currentData?.google?.photos
-
-      }
-    }
-  })
-}
+        google_photos: currentData?.google?.photos,
+      },
+    };
+  });
+};
 
 // searchField and searchValue are for fetching the business data from the Chicago Business API.
 // Examples are address, name, etc
 // https://dev.socrata.com/foundry/data.cityofchicago.org/r5kz-chrr
 const main = async (searchField, searchValue) => {
   // Since the chicago business api is a little weird, the main method has an optional parameter
-  const businessData = await fetchFromChicagoBiz(searchField, searchValue, desiredChicagoBizFields)
+  let businessData = await fetchFromChicagoBiz(searchField, searchValue) //desiredChicagoBizFields)
     .then((allBizData) => {
       // Change doing_business_as_name to name
-      return allBizData.map((singleBizData) => {
-        singleBizData = {chicago: singleBizData};
-        singleBizData.name = singleBizData.chicago.doing_business_as_name;
-        singleBizData.address = singleBizData.chicago.address;
-        return singleBizData;
-      });
+      return allBizData
+        .map((singleBizData) => {
+          singleBizData = { chicago: singleBizData };
+          singleBizData.name = singleBizData.chicago.doing_business_as_name;
+          singleBizData.address = singleBizData.chicago.address;
+          return singleBizData;
+        })
+        .filter(
+          (value, index, self) =>
+            self.findIndex(
+              (yeet) =>
+                yeet.name.toLowerCase().replace(/\W+/g, "") ===
+                value.name.toLowerCase().replace(/\W+/g, "")
+            ) === index
+        );
     })
     .then((allBizData) =>
       // call yelp and google for all of the zip code outputs
       Promise.all(
-        // Temporary slice for testing (do not )
-        allBizData.slice(0, 10).map(async (singleBizData, index) => {
+        allBizData.slice(0, 200).map(async (singleBizData, index) => {
           console.log(`Calling: ${singleBizData.name}`);
 
-          return delay(3000 * (index + 1)).then(() => 
-              fetchFromGoogle(singleBizData, desiredGoogleFields)
-                .then(
-                  async (singleBizDataPlusGoogle) =>
-                    fetchFromYelp(singleBizDataPlusGoogle, desiredYelpFields).catch((err) =>
-                    console.error(`died in yelp: ${err.message}: ${singleBizDataPlusGoogle.chicago.name}`)
+          return delay(3000 * (index + 1)).then(() =>
+            // fetchDescriptionFromGoogleSearch(singleBizData)
+            fetchFromYelp(singleBizData, desiredYelpFields)
+              .then(async (singleBizDataPlusYelp) => {
+                if (singleBizDataPlusYelp.yelp === undefined) {
+                  return singleBizDataPlusYelp;
+                }
+                return fetchFromGoogle(
+                  singleBizDataPlusYelp,
+                  desiredGoogleFields
+                ).catch((err) =>
+                  console.error(
+                    `died in google: ${err.message}: ${singleBizDataPlusYelp.chicago.name}`
                   )
-                )
-                .catch((err) =>
-                  console.error(`died in google: ${err.message}: ${singleBizData}`)
-                )
+                );
+              })
+              .catch((err) =>
+                console.error(`died in yelp: ${err.message}: ${singleBizData}`)
+              )
           );
         })
       )
@@ -145,20 +164,26 @@ const main = async (searchField, searchValue) => {
 
   // // In case we need to print the data
   // printFoundBusinessData(foundBusinessData);
+  businessData = businessData.filter((x) => x?.yelp?.image_url);
 
-  
-  
-  
-  const categorizedBusinessData = buildCategories(businessData);
+  let categorizedBusinessData = buildCategories(businessData);
 
   // console.log(categorizedBusinessData);
+  categorizedBusinessData = await Promise.all(
+    categorizedBusinessData.map(async (bizData) => {
+      return fetchDescriptionFromYelpPage(bizData).then((desc) => {
+        bizData.description = desc;
+        return bizData;
+      });
+    })
+  );
 
   categorizedBusinessData.forEach(x => console.log(JSON.stringify(x, null, 2)));
+  // categorizedBusinessData.forEach((x) => console.log(x.description));
+
   console.log("done");
 
-  return categorizedBusinessData
-  
-  ;
+  return categorizedBusinessData;
 };
 
 // replace mcdonalds with any other business, mcdonalds is just for testing
